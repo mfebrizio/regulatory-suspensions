7acf51eb22442c007869e24f9f4dcbdf7ab6ec2e
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-01-31 16:02:06 -0500

    create preprocessing module

---
+++ code/preprocessing/presidents.py
@@ -0,0 +1,26 @@
+# import dependencies
+from pandas import DataFrame
+
+
+def clean_president_column(df_input: DataFrame, 
+                            column: str = "president"):
+    """Clean column containing president metadata for Federal Register API data.
+
+    Args:
+        df_input (DataFrame): Input dataframe.
+        column (str, optional): Column containing president metadata. Defaults to "president".
+
+    Returns:
+        DataFrame: Copy of df_input with new column, "president_id".
+    """    
+    # create deep copy of input dataframe
+    df = df_input.copy(deep=True)
+    
+    # extract president identifier
+    df["president_id"] = df.apply(lambda x: x[column]["identifier"], axis=1)
+    president_list = list(set(df["president_id"].values.tolist()))
+    print(", ".join(president_list))
+
+    # return output df with new columns
+    return df
+
