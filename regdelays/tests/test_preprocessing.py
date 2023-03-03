aa9b5707af07ab14795b9063836e0ac55d80c918
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-03-03 18:29:22 -0500

    Update test_preprocessing.py

--- regdelays/tests/test_preprocessing.py
+++ regdelays/tests/test_preprocessing.py
@@ -7,16 +7,19 @@ from utils import load_json, load_csv
 
 # load data
 p = Path(__file__)
-test_dir = p.parent.joinpath("files")
-df = load_csv("test_data.csv", test_dir)
+test_dir = p.parents[2].joinpath("data", "raw")
+file = next(f for f in os.listdir(test_dir) if "documents" in f)
+df = load_json(file, test_dir)
+#test_dir = p.parent.joinpath("files")
+#df = load_csv("test_data.csv", test_dir)
 file = next(f for f in os.listdir(test_dir) if "agencies" in f)
-metadata = load_json(file, test_dir, has_metadata=False)
+metadata = load_json(file, test_dir, as_dataframe=False, has_metadata=False)
 
 
 # %% temp
 
-from preprocessing import clean_agencies_column, DEFAULT_AGENCY_SCHEMA
-df2 = clean_agencies_column(df, metadata, schema=DEFAULT_AGENCY_SCHEMA)
+from preprocessing import clean_agencies_column
+df2 = clean_agencies_column(df, metadata)
 
 
 # %%
