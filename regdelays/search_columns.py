32078aa86923e77c554d6287826dd510da293e9a
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-03-20 19:22:01 -0400

    Update search_columns.py
    fix docstrings

--- regdelays/search_columns.py
+++ regdelays/search_columns.py
@@ -20,9 +20,9 @@ def search_columns(df,
         re_flags (optional): Regex flags to use. Defaults to re.I | re.X.
 
     Raises:
-        TypeError: _description_
-        Exception: _description_
-        Exception: _description_
+        TypeError: Raises exception when `patterns` or `columns` parameters are not lists.
+        ValueError: Raises exception when `patterns` or `columns` parameters have incorrect length.
+        ValueError: Raises exception when `return_as` parameter receives an incorrect value.
 
     Returns:
         DataFrame: DataFrame with "indicator" column or filtered by search terms.
@@ -31,12 +31,12 @@ def search_columns(df,
     bool_list = []
     
     # ensure that input patterns and columns are formatted as lists
-    if type(patterns)==list and type(columns)==list:
+    if type(patterns) == list and type(columns) == list:
         pass
     else:
         raise TypeError('Inputs for "patterns" and "columns" keywords must be lists.')
         
-    if len(patterns)==len(columns):
+    if len(patterns) == len(columns):
         # create list of inputs in format [(pattern1, column1),(pattern2, column2), ...]
         inputs = list(zip(patterns,columns))
         
@@ -46,7 +46,7 @@ def search_columns(df,
             searchbool = np.array([True if n==True else False for n in searchre])
             bool_list.append(searchbool)
         
-    elif len(patterns)==1 and (len(patterns)!=len(columns)):
+    elif (len(patterns) == 1) and (len(patterns) != len(columns)):
         # create list of inputs in format [(pattern, column1),(pattern, column2), ...]
         inputs = list(itertools.product(patterns, columns))
         
@@ -57,27 +57,26 @@ def search_columns(df,
             bool_list.append(searchbool)
            
     else:  # eg, patterns formatted as a list of len(n>1) but does not match len(columns)
-        raise Exception("Length of inputs are incorrect. Lengths of 'patterns' and 'columns' must match " +
-        "or a single pattern can map to multiple columns.")
+        raise ValueError("Length of inputs are incorrect. Lengths of 'patterns' and 'columns' must match or a single pattern can map to multiple columns.")
 
     # combine each "searchbool" array elementwise
     # we want a positive match for any column to evaluate as True
     # equivalent to (bool_list[0] | bool_list[1] | bool_list[2] | ... | bool_list[n-1])
     filter_bool = np.array(bool_list).any(axis=0)
 
-    if return_as=="indicator_column":
+    if return_as == "indicator_column":
         dfResults = df.copy(deep=True)
         dfResults.loc[:, "indicator"] = 0
         dfResults.loc[filter_bool, "indicator"] = 1
         print(f"Count: {sum(dfResults['indicator'].values)}")
         return dfResults
     
-    elif return_as=="filtered_df":
+    elif return_as == "filtered_df":
         # filter results
         dfResults = df.loc[filter_bool,:].copy(deep=True)
         print(f"Count: {len(dfResults)}")
         return dfResults
     
     else:
-        raise Exception("Incorrect input for 'return_as' parameter.")
+        raise ValueError("Incorrect input for 'return_as' parameter.")
 
